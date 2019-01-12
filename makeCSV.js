52d9d445ae8794e4d61fa1cd155f5bca8d1cb209
gizmo <gizmo@Gremlin.local>
2019-01-12 13:45:06 -0600

    added makeCSV function

---
+++ makeCSV.js
@@ -0,0 +1,19 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const makeCSV = () => {
+    let string = 'id, first_name, last_name, job_description, image_url';
+    for (let i = 0; i < 10000000; i++) {
+        string += '\n' + i + ', ' + faker.name.firstName() + ', ' + faker.name.lastName() + ', ' + faker.name.jobDescriptor() + ' ' + faker.name.jobType() + ', ' + faker.image.imageUrl()
+    }
+    return string;
+}
+
+const data = makeCSV();
+
+fs.writeFile('pet-data.csv', data, (error) => {
+    if (error) {
+        console.log('error writing to csv file');
+    }
+    console.log('success writing to csv file')
+})
