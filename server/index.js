98f2c4d701ab80495274c7297d24ed77e6add75f
gizmo <gizmo@Gremlin.local>
2019-01-12 11:44:09 -0600

    attempting to serve static page in deployment

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const cors = require("cors");
-const app = express();
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const path = require("path");
+// const cors = require("cors");
+// const app = express();
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, "../client/dist")));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-const port = process.env.PORT || 8000;
+// const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`listening on port ${port}`);
+// });
+
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+app.listen(3000, () => console.log('Server running on port 3000'))
