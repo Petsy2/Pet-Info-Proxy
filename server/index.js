9302ce29a0b14139263b3095521f7e64a217a40c
gizmo <gizmo@Gremlin.local>
2019-01-12 15:30:23 -0600

    updating deployment

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
