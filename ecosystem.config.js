c87bf8392bca6312b673516dd313f8e6973ec215
gizmo <gizmo@Gremlin.local>
2019-01-12 11:23:20 -0600

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-  apps: [
-    {
-      name: "PetsyProxy",
-      script: "./server/index.js"
-    }
-  ],
+  apps: [{
+    name: 'proxy',
+    script: './server/index.js'
+  }],
   deploy: {
     production: {
-      user: "ubuntu",
-      host: "ec2-3-83-224-102.compute-1.amazonaws.com",
-      key: "~/.ssh/PetsyInfoProxyKeys.pem",
-      ref: "origin/master",
-      repo: "https://github.com/mnutt346/Pet-Info-Proxy.git",
-      path: "/home/ubuntu/Pet-Info-Proxy",
-      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
+      user: 'ubuntu',
+      host: 'ec2-18-223-252-165.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/petsy.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:Petsy2/Pet-Info-Proxy.git',
+      path: '/home/ubuntu/proxy',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
-};
+}
